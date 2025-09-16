6892f48b54a99e8a784ae65dafd83d242e943dbe
pawanchaudhary2006 <pawanchaudhary9257@gmail.com>
2025-09-17 00:19:58 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+int a,b, sum;
+printf("enter a value for a: ");
+scanf("%d", &a);
+printf("enter a value for b: ");
+scanf("%d", &b);
+
+sum = a+b;
+printf("the sum of %d and %d is %d.", a,b,sum);
+return 0;
+} 
